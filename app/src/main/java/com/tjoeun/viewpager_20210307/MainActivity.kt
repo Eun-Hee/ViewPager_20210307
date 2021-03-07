bcb901fe9bc05ac27642c86d6ca9c62a314d54d3
Eun-Hee <hd96010@daum.net>
2021-03-07 13:59:02 +0900

    페이지별 제목 문구

--- app/src/main/java/com/tjoeun/viewpager_20210307/MainActivity.kt
+++ app/src/main/java/com/tjoeun/viewpager_20210307/MainActivity.kt
@@ -3,6 +3,7 @@ package com.tjoeun.viewpager_20210307
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.tjoeun.viewpager_20210307.adapters.MainViewpagerAdepter
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,4 +15,20 @@ class MainActivity : AppCompatActivity() {
         setupEvents()
         setValues()
     }
+
+    private fun setupEvents() {
+
+    }
+
+    private fun setValues() {
+
+        mvpa = MainViewpagerAdepter(supportFragmentManager)
+
+        mainViewpager.adapter = mvpa
+
+//        xml에 배치한 탭레이아웃과, 뷰페이저 연결
+        tabLayout.setupWithViewPager(mainViewpager)
+
+    }
+
 }
